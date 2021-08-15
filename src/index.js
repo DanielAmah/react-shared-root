29da92e05b6f8454d632b11974c4678b7fb30ba6
DanielAmah <amahdanieljack@gmail.com>
2021-08-15 04:30:03 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+function importBuildTarget() { 
+  if (process.env.REACT_APP_BUILD_TARGET === "app") { 
+    return import("./app.js"); 
+  } else if (process.env.REACT_APP_BUILD_TARGET === "test") { 
+    return import("./test.js"); 
+  } else { 
+    return Promise.reject(
+      new Error("No such build target: " + process.env.REACT_APP_BUILD_TARGET)
+    ); 
+  } 
+} 
+
+importBuildTarget().then(({ default: Environment }) => 
+  ReactDOM.render( 
+    <React.StrictMode> 
+      <Environment /> 
+    </React.StrictMode>
+  , document.getElementById("root") 
+  ) 
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
